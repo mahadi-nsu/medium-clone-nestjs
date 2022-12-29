bab7a970d638928e1c9662654b75c27a0a492f97
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-12-29 17:43:20 +0600

    Validation added for dtos

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,16 @@
 import { Controller, Post } from '@nestjs/common';
-import { Body } from '@nestjs/common/decorators';
+import { Body, UsePipes } from '@nestjs/common/decorators';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserService } from './user.service';
 import { UserResponseInterface } from './types/userResponse.interface';
+import { ValidationPipe } from '@nestjs/common/pipes';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
   @Post('users')
+  @UsePipes(new ValidationPipe({ transform: true }))
   async createUser(
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<UserResponseInterface> {
